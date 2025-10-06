8d7465cf7f2510f01f243015d834fac9524106e1
rokezovwas <you@example.com>
2025-10-06 18:50:52 +0300

    first commit

--- script.py
+++ script.py
@@ -1,21 +1,3 @@
-from telethon import TelegramClient
-
-api_id = 26753991
-api_hash = '91ed1cdb2cb8b6018d0354106bf11ad9'  
-session_name = 'userbot' 
-
-client = TelegramClient(session_name, api_id, api_hash)
-
-async def main():
-    me = await client.get_me()
-    print("Подключились как:", me.username)
-
-with client:
-    client.loop.run_until_complete(main())
-
-
-
-
 from telethon import TelegramClient, errors
 import asyncio
 import time
@@ -31,33 +13,68 @@ async def main():
     me = await client.get_me()
     print("Подключились как:", me.username)
 
-    text = """📢 Орендую ваші карти
-
-💵 ПУМБ — 1500
-💵 УкрСиб — 750
-💵 Акорд — 500
-💵 FreeBank — 500
-💵 Альянс (ліміт 50k) — 500
-💵 MonoBank 14+ — 800–1000
-
-✨ Є багато відгуків — все можна переглянути в Telegram (біо).
-
-✍️ Писати: @user2020219"""
+    text = """RentBiz 💥  — Надійний партнер покупки / оренди банків.
 
 
+
+💸НАШ ПРАЙС:
+
+🏦 PUMB  😎
+(18+ років - рег. ) - 1️⃣5️⃣0️⃣0️⃣
+
+🏦 Credit Agricole  😎
+(18+ років - рег.) - 7️⃣0️⃣0️⃣
+
+🏦 FreeBank  😎
+(18+ років - рег.) - 7️⃣0️⃣0️⃣
+
+🏦 UKRSSIB 2.0  😎
+(18+ років - рег.) - 5️⃣0️⃣0️⃣
+
+ 🏦Райфайзен 😎
+(18+ років - рег.) - 5️⃣0️⃣0️⃣
+
+🏦 Монобанк  😎
+(14+ років .) -  1️⃣3️⃣0️⃣0️⃣
+
+
+📱 ЗВʼЯЗОК: @user2020219 
+
+
+
+💳ОПЛАТА ГАРАНТОВАНА ПРОТЯГОМ 1години 💳
+
+🎁БОНУС ЗА ПРИВЕДЕНОГО ДРУГА.
+
+🤝ШУКАЮ ДРОПОВОДІВ!📞
+
+⏰ГРАФІК РОБОТИ:
+С 7.00 до 00.00 по Києву — 6/1
+Наш канал з відгуками та актуальними цінами: https://t.me/orendam1
+ """
 
     dialogs = await client.get_dialogs()  # Получаем все диалоги
 
     for dialog in dialogs:
         try:
-            # Фильтруем: отправляем только пользователям и группам
-            if dialog.is_channel or dialog.is_group:
+            # Пропускаем RentBiz
+            if dialog.name == "RentBiz":
+                print(f"Пропущен: {dialog.name}")
+                continue
+
+            # Если обычная группа
+            if dialog.is_group:
                 await client.send_message(dialog.entity, text)
-                print(f"Отправлено в: {dialog.name}")
+                print(f"Отправлено в обычную группу: {dialog.name}")
                 time.sleep(random.uniform(1, 3))
-        except errors.FloodWaitError as e:
-            print(f"Нужно подождать {e.seconds} секунд")
-            time.sleep(e.seconds + 1)
+                continue
+
+            # Если супергруппа (канал, но с megagroup=True)
+            if dialog.is_channel and getattr(dialog.entity, "megagroup", False):
+                await client.send_message(dialog.entity, text)
+                print(f"Отправлено в супергруппу: {dialog.name}")
+                time.sleep(random.uniform(1, 3))
+
         except Exception as e:
             print(f"Ошибка при отправке в {dialog.name}: {e}")
             time.sleep(1)
